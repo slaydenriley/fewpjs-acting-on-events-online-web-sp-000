b8ed08f7635823ee9690b093f06d09643519aa3f
Riley Slayden <slaydenriley@gmail.com>
2020-05-04 14:58:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
 
-  if (right > 0) {
+  if (right < 0) {
     dodger.style.right = `${right + 1}px`;
   }
 }
